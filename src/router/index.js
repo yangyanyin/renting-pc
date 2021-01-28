d87c2a116a211969e7a867c13bac9216cc5ca25f
yangyanyin <yangyanyin@aliyun.com>
2021-01-28 14:03:03 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+const Home = () => import(/* webpackChunkName: "home" */ '../views/home/Home')
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
+
+export default router
