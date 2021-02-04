221ae93383c5e10464e0a002f2cf63ccdc8c9b72
yangyanyin <yangyanyin@aliyun.com>
2021-02-04 10:51:36 +0800

    estate

--- src/views/estate/EstateIndex.vue
+++ src/views/estate/EstateIndex.vue
@@ -2,13 +2,39 @@
   <div class="estate">
     <div class="banner">
       <div class="w1200px btn">
-        <button> <img src="../../assets/image/estate-b-icon1.png" alt="新加坡商业地产"/> 新加坡商业地产</button>
-        <button> <img src="../../assets/image/estate-b-icon2.png" alt="分层地契商业办公大楼"/> 分层地契商业办公大楼</button>
-        <button> <img src="../../assets/image/estate-b-icon3.png" alt="保留性店屋"/> 保留性店屋</button>
+        <router-link tag="button" to="/"> <img src="../../assets/image/estate-b-icon1.png" alt="新加坡商业地产"/> 新加坡商业地产</router-link>
+        <router-link tag="button" to="/"> <img src="../../assets/image/estate-b-icon2.png" alt="分层地契商业办公大楼"/> 分层地契商业办公大楼</router-link>
+        <router-link tag="button" to="/"> <img src="../../assets/image/estate-b-icon3.png" alt="保留性店屋"/> 保留性店屋</router-link>
       </div>
     </div>
 
     <Introduction v-for="(item, k) in introductionData" :key="k" :introduction="item" />
+
+    <div class="advantage w1200px mt80">
+      <h3>投资商业地产的优势</h3>
+      <ul class="clearfix">
+        <li>
+          <img src="../../assets/image/estate-advantage-icon1.png" alt="">
+          <p>无购买身份限制 <br /> 外国人可以买 <br /> 无额外印花税</p>
+        </li>
+        <li>
+          <img src="../../assets/image/estate-advantage-icon2.png" alt="">
+          <p>随时自由买卖 <br /> 不受私宅卖方印花税条例限制</p>
+        </li>
+        <li>
+          <img src="../../assets/image/estate-advantage-icon3.png" alt="">
+          <p>银行给予最高 <br /> 商业贷款可高达80%(附带条件)</p>
+        </li>
+        <li>
+          <img src="../../assets/image/estate-advantage-icon4.png" alt="">
+          <p>地理位置极其优越 <br /> 位于寸土寸金的金融中心，保值增值性极高</p>
+        </li>
+        <li>
+          <img src="../../assets/image/estate-advantage-icon5.png" alt="">
+          <p>商业的款利率低，租金回报率高，可以以租养贷，没有房贷压力，这类地产租金回报高</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
@@ -51,12 +77,52 @@ export default {
     border: 1px solid #E8E7E7;
     border-radius: 5px;
     background: no-repeat;
+    transition: .3s;
+    cursor: pointer;
     &:last-child {
       margin-right: 0;
+    }
+    &:hover {
+      background: #24A10F;
+      border-color: #24A10F;
+      color: #fff;
+      img {
+        transform: translateY(-5px) scale(1.1);
+      }
     }
     img {
       display: inline-block;
       width: 70px;
+      transition: .3s;
+    }
+  }
+}
+.advantage {
+  text-align: center;
+  h3 {
+    font-size: 24px;
+    color: #24A10F;
+  }
+  ul {
+    padding-top: 50px;
+    li {
+      float: left;
+      width: 224px;
+      height: 307px;
+      margin-right: 20px;
+      padding: 0 30px;
+      border: 1px solid #D6D6D6;
+      border-radius: 10px;
+      &:last-child {
+        margin-right: 0;
+      }
+      p {
+        line-height: 24px;
+      }
+      img {
+        width: 120px;
+        margin: 40px auto;
+      }
     }
   }
 }
