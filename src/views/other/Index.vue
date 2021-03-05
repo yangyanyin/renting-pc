23c37472d3010c49cb25da95eacef6ce3eb7b458
yangyanyin <yangyanyin@aliyun.com>
2021-03-05 17:30:26 +0800

    about

--- src/views/other/Index.vue
+++ src/views/other/Index.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="other">
     <BreadcrumbList />
-    <div class="content clearfix w1200px">
+    <div class="clearfix w1200px mt40">
       <div class="left menu">
         <router-link to="/about">关于我们</router-link>
         <router-link to="/contact-us">联系我们</router-link>
         <router-link to="/copyright">版权声明</router-link>
       </div>
-      <div class="right">
+      <div class="right content">
         <router-view></router-view>
       </div>
     </div>
@@ -22,3 +22,31 @@ export default {
   }
 }
 </script>
+<style scoped lang="less">
+.menu {
+  width: 260px;
+  padding: 5px 0 32px;
+  border: 1px solid #F5F5F5;
+  a {
+    display: block;
+    height: 20px;
+    margin-top: 25px;
+    padding-left: 30px;
+    font-size: 16px;
+    line-height: 20px;
+    border-left: 4px solid #fff;
+    &:hover {
+      color: #24A10F;
+    }
+    &.router-link-active {
+      color: #24A10F;
+      border-color: #24A10F;
+    }
+  }
+}
+.content {
+  width: 910px;
+  padding: 30px;
+  background: #F5F5F5;
+}
+</style>
